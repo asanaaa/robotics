566d1ad9566e6710e3c368ab959ce600bfa815b0
Chernysheva Arina <a.chernysheva1@g.nsu.ru>
2025-09-20 19:19:25 +0700

    fix: add topic logic

--- ex10/text_to_cmd_vel/text_to_cmd_vel/turtle_pub.py
+++ ex10/text_to_cmd_vel/text_to_cmd_vel/turtle_pub.py
@@ -1,13 +1,24 @@
 import rclpy
 from rclpy.node import Node
 
+from custom_msg.msg import Command
 from geometry_msgs.msg import Twist, Vector3
 
 
-class MinimalPublisher(Node):
+class ControlNode(Node):
 
     def __init__(self):
+        super().__init__('minimal_subscriber')
         super().__init__('minimal_publisher')
+        # Subscriber
+        self.subscription = self.create_subscription(
+            Command,
+            'cmd_text',
+            self.listener_callback,
+            10)
+        self.subscription
+
+        # Publisher
         self.publisher_ = self.create_publisher(Twist, 'turtle1/cmd_vel', 10)
         self.msg = {
             "turn_right": Twist(angular=Vector3(x=0.0, y=0.0, z=-1.6)),
@@ -15,25 +26,41 @@ class MinimalPublisher(Node):
             "move_forward": Twist(linear=Vector3(x=1.0, y=0.0, z=0.0)),
             "move_backward": Twist(linear=Vector3(x=-1.0, y=0.0, z=0.0)),
         }
-        print(f'Use the commands below to move the turtle.\nturn_right | turn_left | move_forward | move_backward\n')
-        while True:
-            self.publish()
 
-    def publish(self):
-        command = input()
-        if self.msg.get(command, None):
-            self.publisher_.publish(self.msg[command])
-            self.get_logger().info('Publishing: "%s"' % self.msg[command])
+    def listener_callback(self, msg):
+        self.publisher_.publish(self.msg[msg.command])
+        self.get_logger().info('Publishing: "%s"' % self.msg[msg.command])
+        
+# class MinimalPublisher(Node):
+
+#     def __init__(self):
+#         super().__init__('minimal_publisher')
+#         self.publisher_ = self.create_publisher(Twist, 'cmd_text', 10)
+#         self.msg = {
+#             "turn_right": Twist(angular=Vector3(x=0.0, y=0.0, z=-1.6)),
+#             "turn_left": Twist(angular=Vector3(x=0.0, y=0.0, z=1.6)),
+#             "move_forward": Twist(linear=Vector3(x=1.0, y=0.0, z=0.0)),
+#             "move_backward": Twist(linear=Vector3(x=-1.0, y=0.0, z=0.0)),
+#         }
+#         print(f'Use the commands below to move the turtle.\nturn_right | turn_left | move_forward | move_backward\n')
+#         while True:
+#             self.publish()
+
+#     def publish(self):
+#         command = input()
+#         if self.msg.get(command, None):
+#             self.publisher_.publish(self.msg[command])
+#             self.get_logger().info('Publishing: "%s"' % self.msg[command])
 
 
 def main(args=None):
     rclpy.init(args=args)
 
-    minimal_publisher = MinimalPublisher()
+    control_node = ControlNode()
 
-    rclpy.spin(minimal_publisher)
+    rclpy.spin(control_node)
 
-    minimal_publisher.destroy_node()
+    control_node.destroy_node()
     rclpy.shutdown()
 
 
