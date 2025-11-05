5467c9357af5466b52a4c5c12bb87c0f7151fec3
Chernysheva Arina <a.chernysheva1@g.nsu.ru>
2025-11-05 15:50:22 +0700

    feat: add task 3

---
+++ ex04/launch/turtle_tf2_dynamic_frame_demo_launch.py
@@ -0,0 +1,37 @@
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
+from launch.substitutions import PathJoinSubstitution, LaunchConfiguration
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+
+def generate_launch_description():
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            'radius',
+            default_value='2.0',
+            description='Radius of rotation for carrot around turtle1'
+        ),
+        DeclareLaunchArgument(
+            'direction_of_rotation',
+            default_value='1',
+            description='Direction of rotation: 1 for clockwise, -1 for counter-clockwise'
+        ),
+        
+        IncludeLaunchDescription(
+            PathJoinSubstitution([
+                FindPackageShare('learning_tf2_py'), 'launch', 'turtle_tf2_demo_launch.py'
+            ]),
+            launch_arguments={'target_frame': 'carrot1'}.items(),
+        ),
+        
+        Node(
+            package='learning_tf2_py',
+            executable='dynamic_frame_tf2_broadcaster',
+            name='dynamic_broadcaster',
+            parameters=[{
+                'radius': LaunchConfiguration('radius'),
+                'direction_of_rotation': LaunchConfiguration('direction_of_rotation')
+            }]
+        ),
+    ])
